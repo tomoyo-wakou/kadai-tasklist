c10d2592717437c8359bdd4fe32c9cb0b4a79b77
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-12-03 16:29:43 +0000

    user registration

---
+++ database/migrations/2021_12_03_235611_add_user_id_to_tasks.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger("user_id");
+            
+            // 外部キー制約
+            $table->foreign("user_id")->references("id")->on("tasks");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            // 外部キー制約解除
+            $table->dropForeign(["user_id"]);
+            $table->dropColumn("user_id");
+        });
+    }
+}
