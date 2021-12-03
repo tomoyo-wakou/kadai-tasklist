0fcbf5011c594d2ce9ecb13f58c7acd886f8cbbd
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-12-03 14:34:55 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,32 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
 
-<!-- ここにページ毎のコンテンツを書く -->
-    <h1>タスク一覧</h1>
+            <!-- ここにページ毎のコンテンツを書く -->
+            <h1>タスク一覧</h1>
     
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    {{-- タスク詳細ページへのリンク --}}
-                    <td>{{ link_to_route("tasks.show", $task->id, ["task" => $task->id]) }}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+            @if (count($tasks) > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                                <th>タスク</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            {{-- タスク詳細ページへのリンク --}}
+                                <td>{{ link_to_route("tasks.show", $task->id, ["task" => $task->id]) }}</td>
+                                <td>{{ $task->status }}</td>
+                                <td>{{ $task->content }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+    
+                {{-- メッセージ作成ページへのリンク --}}
+                {!! link_to_route("tasks.create", "新規タスクの投稿", [], ["class" => "btn btn-primary"]) !!}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+            </div>
+        </div>
     @endif
-    
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route("tasks.create", "新規タスクの投稿", [], ["class" => "btn btn-primary"]) !!}
-
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get("/", "TasksController@index");
 
 Route::resource("tasks", "TasksController");
+
+Route::get("login", "Auth\LoginController@showLoginForm")->name("login");
+Route::post("login", "Auth\LoginController@login")->name("login.post");
+Route::get("logout", "Auth\LoginController@logout")->name("logout.get");
