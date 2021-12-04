1c25c407a8a7f60509e66eda1feb520419de9e92
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-12-04 12:14:49 +0000

    fix taskscontroller 4

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Auth::check())
     
         <!-- ここにページ毎のコンテンツを書く -->
         <h1>タスク新規作成ページ</h1>
@@ -25,14 +24,5 @@
                 {!! Form::close() !!}
             </div>
     </div>
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
-                {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
-        </div>
-    @endif
 
 @endsection
