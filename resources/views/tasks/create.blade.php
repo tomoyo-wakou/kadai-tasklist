e28df5eb9980ff7c6a74e6e98f8d12158e473d41
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-12-04 12:04:35 +0000

    fix taskscontroller 3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,29 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-
-<!-- ここにページ毎のコンテンツを書く -->
-    <h1>タスク新規作成ページ</h1>
+    @if (Auth::check())
     
-    <div class="row">
-        <div class="col-6">
-            {!! Form::model($task, ["route" => "tasks.store"]) !!}
+        <!-- ここにページ毎のコンテンツを書く -->
+        <h1>タスク新規作成ページ</h1>
+    
+        <div class="row">
+            <div class="col-6">
+                {!! Form::model($task, ["route" => "tasks.store"]) !!}
             
-            <div class="form-group">
-                {!! Form::label("status", "ステータス：") !!}
-                {!! Form::text("status", null, ["class" => "form-control"]) !!}
+                <div class="form-group">
+                    {!! Form::label("status", "ステータス：") !!}
+                    {!! Form::text("status", null, ["class" => "form-control"]) !!}
+                </div>
+            
+                <div class="form-group">
+                    {!! Form::label("content", "タスク：") !!}
+                    {!! Form::text("content", null, ["class" => "form-control"]) !!}
+                </div>
+            
+                {!! Form::submit("投稿", ["class" => "btn btn-primary"]) !!}
+            
+                {!! Form::close() !!}
             </div>
-            
-            <div class="form-group">
-                {!! Form::label("content", "タスク：") !!}
-                {!! Form::text("content", null, ["class" => "form-control"]) !!}
-            </div>
-            
-            {!! Form::submit("投稿", ["class" => "btn btn-primary"]) !!}
-            
-            {!! Form::close() !!}
-        </div>
-        
     </div>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
 
 @endsection
